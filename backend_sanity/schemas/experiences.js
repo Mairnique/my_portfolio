9e8b08029df896410378904d94127e3a23801f2c
Mairnique <mchu5522@gmail.com>
2022-07-11 10:32:05 -0400

    first commit

---
+++ backend_sanity/schemas/experiences.js
@@ -0,0 +1,24 @@
+export default{
+    name:'experiences',
+    title:'Experiences',
+    type: 'document',
+    fields:[
+        {
+            name:'year',
+            title:'Year',
+            type:'string'
+        },
+        {
+            name:'works',
+            title:'Works',
+            type:'array',
+            of:[{ type:'workExperience'}]
+        },
+        {
+            name: "order",
+            title: "Order",
+            type: "number",
+            hidden: true,
+        },
+    ]
+}
